8e735ec43d48dcf8ca949c267fa3217b59dabf50
hieu24313 <2051050138hieu@ou.edu.com>
2023-04-07 16:20:14 +0700

    update end database

--- csdlsaleapp.sql
+++ csdlsaleapp.sql
@@ -35,7 +35,7 @@ CREATE TABLE `chinhanh` (
 
 LOCK TABLES `chinhanh` WRITE;
 /*!40000 ALTER TABLE `chinhanh` DISABLE KEYS */;
-INSERT INTO `chinhanh` VALUES (1,'Hà Nội'),(2,'Hồ Chí Minh'),(3,'Đà Nẵng'),(4,'Đồng Nai');
+INSERT INTO `chinhanh` VALUES (1,'HÃ  Ná»™i'),(2,'Há»“ ChÃ­ Minh'),(3,'ÄÃ  Náºµng'),(4,'Äá»“ng Nai'),(5,'Háº£i PhÃ²ng'),(6,'BÃ¬nh Äá»‹nh'),(7,'Gia Lai'),(8,'Nha Trang');
 /*!40000 ALTER TABLE `chinhanh` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,7 +66,7 @@ CREATE TABLE `chitiethd` (
 
 LOCK TABLES `chitiethd` WRITE;
 /*!40000 ALTER TABLE `chitiethd` DISABLE KEYS */;
-INSERT INTO `chitiethd` VALUES (1,1,1,2,18000),(2,1,2,1,15000),(3,2,3,3,45000),(4,2,4,2,10000),(5,3,5,4,24000),(6,3,6,1,27000),(7,4,7,3,75000),(8,4,8,2,24000),(9,5,9,2,36000),(10,5,10,1,90000);
+INSERT INTO `chitiethd` VALUES (1,1,1,2,18000),(2,1,2,1,15000),(3,2,3,3,45000),(4,2,4,2,10000),(5,3,5,4,24000),(6,3,6,1,27000),(7,4,7,3,75000),(8,4,8,2,24000),(9,5,9,2,36000),(10,5,10,1,90000),(11,10,2,1,15000),(12,10,1,100,1000000),(13,11,2,1,15000),(14,11,1,100,1000000),(15,11,1,1,10000),(16,11,2,12,180000),(17,11,3,1,20000),(18,11,4,1,5000),(19,11,5,1,8000),(20,11,6,1,30000),(21,11,7,1,25000);
 /*!40000 ALTER TABLE `chitiethd` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,7 +92,7 @@ CREATE TABLE `giamgia` (
 
 LOCK TABLES `giamgia` WRITE;
 /*!40000 ALTER TABLE `giamgia` DISABLE KEYS */;
-INSERT INTO `giamgia` VALUES (0,0.1,'2022-03-01','2022-03-31'),(1,0.1,'2022-01-01','2022-01-31'),(2,0.2,'2022-02-01','2022-02-28'),(3,0.6,'2022-03-01','2022-03-31');
+INSERT INTO `giamgia` VALUES (0,0,'2020-03-01','2030-03-31'),(1,0.1,'2022-01-01','2022-01-31'),(2,0.2,'2022-02-01','2022-02-28'),(3,0.6,'2022-03-01','2022-03-31'),(4,0.1,'2023-03-27','2023-04-06'),(5,0.3,'2023-04-05','2023-04-05'),(6,0.3,'2023-04-05','2023-04-05'),(7,0.3,'2023-04-05','2023-04-05'),(8,0.9,'2023-04-05','2023-04-05'),(9,0.9,'2023-04-05','2023-04-05'),(10,1,'2023-04-06','2023-04-14');
 /*!40000 ALTER TABLE `giamgia` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,7 +127,7 @@ CREATE TABLE `hoadon` (
 
 LOCK TABLES `hoadon` WRITE;
 /*!40000 ALTER TABLE `hoadon` DISABLE KEYS */;
-INSERT INTO `hoadon` VALUES (1,1,1,1,100000,120000,'2022-01-01'),(2,2,2,2,50000,55000,'2022-01-02'),(3,3,3,3,80000,90000,'2022-01-03'),(4,1,2,2,120000,130000,'2022-01-04'),(5,2,3,1,60000,65000,'2022-01-05'),(6,2,2,1,1015000,2141240000,'2023-03-30'),(7,2,2,1,10000,142355,'2023-03-30'),(8,2,2,1,10500,2542540,'2023-03-30'),(9,2,2,1,25000,434362,'2023-03-30'),(10,2,2,1,25000,141414,'2023-03-30'),(11,2,2,1,0,413451,'2023-03-30'),(12,2,2,1,25000,4325240,'2023-03-30'),(13,2,2,1,30100000,55555600000,'2023-03-30'),(14,2,2,1,15010000,15100000,'2023-03-30'),(15,2,2,1,115000,115000,'2023-03-31'),(16,2,2,4,160000,1600000,'2023-03-31'),(17,2,2,1,160000,1600000,'2023-03-31'),(18,2,2,1,45000,45000,'2023-03-31'),(19,2,2,2,25000,2,'2023-03-31'),(20,2,2,1,25000,25000,'2023-03-31'),(21,2,2,3,25000,25000,'2023-03-31'),(22,2,2,3,25000,25000,'2023-03-31'),(23,2,2,1,135000,135000,'2023-04-01'),(24,2,2,4,45000,45000,'2023-04-01'),(25,2,2,1,45000,100000,'2023-04-01'),(26,2,2,1,25000,25000,'2023-04-01'),(27,2,2,1,25000,321,'2023-04-01'),(28,2,2,1,243000,321,'2023-04-01'),(29,2,2,1,25000,23,'2023-04-01'),(30,2,2,1,25000,23,'2023-04-01'),(31,2,2,1,25000,321,'2023-04-01'),(32,2,2,1,25000,321,'2023-04-01'),(33,2,2,1,45000,45000,'2023-04-01'),(34,2,2,1,45000,45000,'2023-04-01'),(35,2,2,1,45000,45000,'2023-04-01'),(36,2,2,1,45000,412,'2023-04-01'),(37,2,2,1,45000,412,'2023-04-01'),(38,2,2,1,1500000000000,8.65866e18,'2023-04-01'),(39,2,2,1,1500000000000,86586600000000,'2023-04-01'),(40,2,2,1,1500000000000,865866000000000,'2023-04-01'),(41,2,2,1,1500000000000,8.65866e15,'2023-04-01');
+INSERT INTO `hoadon` VALUES (1,1,1,1,100000,120000,'2022-01-01'),(2,2,2,2,50000,55000,'2022-01-02'),(3,3,3,3,80000,90000,'2022-01-03'),(4,1,2,2,120000,130000,'2022-01-04'),(5,2,3,1,60000,65000,'2022-01-05'),(6,2,2,1,1015000,2141240000,'2023-03-30'),(7,2,2,1,10000,142355,'2023-03-30'),(8,2,2,1,10500,2542540,'2023-03-30'),(9,2,2,1,25000,434362,'2023-03-30'),(10,2,2,1,1015000,1015000,'2023-04-05'),(11,2,2,1,1293000,12900000,'2023-04-05');
 /*!40000 ALTER TABLE `hoadon` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,7 +154,7 @@ CREATE TABLE `khachhang` (
 
 LOCK TABLES `khachhang` WRITE;
 /*!40000 ALTER TABLE `khachhang` DISABLE KEYS */;
-INSERT INTO `khachhang` VALUES (1,'Nguyễn Minh','Hiếu','2002-06-22','0359505026'),(2,'Nguyễn Minh','Tuấn','2002-06-22','0359505026'),(3,'Nguyễn Minh','Hải','2002-06-22','0359505026'),(4,'Trần Văn','Liên','2023-03-27','0491041928'),(5,'Nguyễn Minh','Hiếu','2002-06-22','445134651'),(6,'Trương Hoàng','Vinh','2023-04-01','64326'),(7,'Truong H','Vinh','1980-01-01','64326'),(9,'Dương Hữu ','Thành','1990-01-01','012445465'),(10,'hiếu','nguyễn minh','2023-03-27','0574573');
+INSERT INTO `khachhang` VALUES (1,'Nguyá»…n Minh','Hiáº¿u','2002-06-22','0359505026'),(2,'Nguyá»…n Minh','Tuáº¥n','2002-06-22','0359505026'),(3,'Nguyá»…n Minh','Háº£i','2002-06-22','0359505026'),(4,'Tráº§n VÄƒn','LiÃªn','2023-03-27','0491041928'),(5,'Nguyá»…n Minh','Hiáº¿u','2002-06-22','445134651'),(6,'TrÆ°Æ¡ng HoÃ ng','Vinh','2023-04-01','64326'),(7,'Truong H','Vinh','1980-01-01','64326'),(9,'DÆ°Æ¡ng Há»¯u ','ThÃ nh','1990-01-01','012445465'),(10,'hiáº¿u','nguyá»…n minh','2023-03-27','0574573');
 /*!40000 ALTER TABLE `khachhang` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -186,7 +186,7 @@ CREATE TABLE `nhanvien` (
 
 LOCK TABLES `nhanvien` WRITE;
 /*!40000 ALTER TABLE `nhanvien` DISABLE KEYS */;
-INSERT INTO `nhanvien` VALUES (1,'Nguyễn Văn','A',1,'a','1',1),(2,'Trần Thị','B',2,'a1','1',0),(3,'Lê Văn',' C',2,'admin2','123',0),(4,'Phạm Thị','D',3,'admin3','123',1),(5,'Đặng Văn','E',3,'admin4','123',0);
+INSERT INTO `nhanvien` VALUES (1,'Nguyá»…n VÄƒn','A',1,'a','1',1),(2,'Tráº§n Thá»‹','B',2,'a1','1',0),(3,'LÃª VÄƒn',' C',2,'admin2','123',0),(4,'Hiáº¿u','nguyen minh',3,'ad','123',0);
 /*!40000 ALTER TABLE `nhanvien` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -203,10 +203,13 @@ CREATE TABLE `sanpham` (
   `Gia` float DEFAULT NULL,
   `DonVi` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `XuatXu` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `MaChiNhanh` int DEFAULT NULL,
   `MaGiamGia` int DEFAULT NULL,
   PRIMARY KEY (`MaSanPham`),
   KEY `sanpham_ibfk_1` (`MaGiamGia`),
-  CONSTRAINT `sanpham_ibfk_1` FOREIGN KEY (`MaGiamGia`) REFERENCES `giamgia` (`MaGiamGia`) ON DELETE SET NULL
+  KEY `sanpham_ibfk_2` (`MaChiNhanh`),
+  CONSTRAINT `sanpham_ibfk_1` FOREIGN KEY (`MaGiamGia`) REFERENCES `giamgia` (`MaGiamGia`) ON DELETE SET NULL,
+  CONSTRAINT `sanpham_ibfk_2` FOREIGN KEY (`MaChiNhanh`) REFERENCES `chinhanh` (`MaChiNhanh`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -216,7 +219,7 @@ CREATE TABLE `sanpham` (
 
 LOCK TABLES `sanpham` WRITE;
 /*!40000 ALTER TABLE `sanpham` DISABLE KEYS */;
-INSERT INTO `sanpham` VALUES (1,'Sản phẩm A',10000,'Chiếc','VN',1),(2,'Sản phẩm B',15000,'Chiếc','CN',2),(3,'Sản phẩm C',20000,'Chiếc','JP',2),(4,'Sản phẩm D',5000,'Cái','USA',1),(5,'Sản phẩm E',8000,'Cái','UK',3),(6,'Sản phẩm F',30000,'Bộ','UK',1),(7,'Sản phẩm G',25000,'Bộ','VN',3),(8,'Sản phẩm H',12000,'Chiếc','VN',2),(9,'Sản phẩm I',18000,'Chiếc','VN',1),(10,'Sản phẩm J',100000,'Bộ','VN',3);
+INSERT INTO `sanpham` VALUES (1,'Sáº£n pháº©m A',10000,'Chiáº¿c','VN',1,1),(2,'Sáº£n pháº©m B',15000,'Chiáº¿c','CN',2,2),(3,'Sáº£n pháº©m C',20000,'Chiáº¿c','JP',2,2),(4,'Sáº£n pháº©m D',5000,'CÃ¡i','USA',4,1),(5,'Sáº£n pháº©m E',8000,'CÃ¡i','UK',3,3),(6,'Sáº£n pháº©m F',30000,'Bá»™','UK',1,1),(7,'Sáº£n pháº©m G',25000,'Bá»™','VN',2,3),(8,'Sáº£n pháº©m H',12000,'Chiáº¿c','VN',1,2),(9,'Sáº£n pháº©m I',18000,'Chiáº¿c','VN',2,1),(10,'Sáº£n pháº©m J',100000,'Bá»™','VN',2,3),(11,'CÃ  PhÃ¡o',1200,'Cá»§','JP',2,1),(12,'CÃ  Rá»‘t',12900,'Kg','CN',1,0),(13,'CÃ  Chua',32141,'Quáº£','VN',1,0),(14,'Hiáº¿u',100000000000,'NgÆ°á»i','Viá»‡t Nam',2,10);
 /*!40000 ALTER TABLE `sanpham` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -229,4 +232,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-05 15:47:27
+-- Dump completed on 2023-04-07 16:19:09
